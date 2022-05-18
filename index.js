803f4b5bef895a99333ce39829389acefc2c56a2
phaheez <faizfasasi@gmail.com>
2022-05-18 03:09:49 +0100

    working npm package

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const service = require('./lib/kontri_service');
+'use strict';
 
+const service = require('./lib/service');
 /**
  * Country Class
  */
-class Kontri {
+class KontriAPI {
+    #apiKey;
+    #baseURL;
+
+    constructor(APIKEY, environment = 'development') {
+        const apiBase = {
+            sandbox: 'https://restcountries.com/v3.1',
+            live: 'https://restcountries.com/v3.1'
+        };
+
+        this.#apiKey = APIKEY;
+        this.#baseURL = (environment.toLowerCase() === 'production') ? apiBase.live : apiBase.sandbox;
+    }
+
     /**
      * Get All Countries
      * 
      * @returns {Array}
      */
     getAllCountries = async () => {
-        const result = await service.getAll();
-        return result;
+        return await service.getData(`${this.#baseURL}/all`, this.#apiKey);
     }
 
     /**
@@ -21,9 +34,20 @@ class Kontri {
      * @returns {Array}
      */
     getCountryByCode = async (code) => {
-        const result = await service.getByCode(code);
-        return result;
+        return await service.getData(`${this.#baseURL}/alpha/${code}`, this.#apiKey);
+    }
+
+    /**
+     * Add New Country
+     * 
+     * @param   {Object} { name, code }
+     * @returns {Object} { url, name, code }
+     */
+    addCountry = async (data) => {
+        const { name, code } = data;
+        return await service.postData(`${this.#baseURL}/add-country`, this.#apiKey, { name, code });
     }
 }
 
-module.exports = Kontri;
+
+module.exports = KontriAPI;
